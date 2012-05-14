0dd4bbe8871fb3a598e92ad26063e97b6c217549
Espen Moe-Nilssen <espen@medialog.no>
2012-05-14 20:04:18 +0200

    Added files for supersized slideshow

---
+++ README.txt
@@ -0,0 +1,17 @@
+Introduction
+============
+
+Integration of supersized in Plone4(tested) and Plone3 (untested).
+
+This version include supersized v3.2.7
+It was made for using it with collective.plonetruegallery
+
+
+Authors
+=======
+
+- Espen Moe-Nilssen <espen@medialog.no>
+
+Contributors
+
+.. _supersized: http://buildinternet.com/project/supersized
---
+++ collective/js/supersized/tests/base.py
@@ -0,0 +1,33 @@
+"""Test setup for registration and functional tests."""
+
+import sys
+from Products.Five import zcml
+from Products.Five import fiveconfigure
+
+from Testing import ZopeTestCase as ztc
+from Products.PloneTestCase import PloneTestCase as ptc
+from Products.PloneTestCase.layer import onsetup
+
+@onsetup
+def setup_addon():
+    fiveconfigure.debug_mode = True
+    import collective.js.supersized
+    zcml.load_config('configure.zcml', collective.js.supersized)
+    fiveconfigure.debug_mode = False
+    ztc.installPackage('collective.js.supersized')
+
+setup_addon()
+ptc.setupPloneSite(extension_profiles=['collective.js.supersized:default'])
+
+class TestCase(ptc.PloneTestCase):
+    """We use this base class for all the tests in this package. If necessary,
+    we can put common utility or setup code in here. This applies to unit 
+    test cases.
+    """
+
+
+class FunctionalTestCase(ptc.FunctionalTestCase):
+    """We use this class for functional integration tests that use doctest
+    syntax. Again, we can put basic common utility or setup code in here.
+    """
+

edcbf1c6d05541f4aefb3a0d04da9dc1a7b60535
Espen Moe-Nilssen <espen@medialog.no>
2012-05-17 15:38:15 +0200

    Prepare collective.js.supersized 0.1dev.

--- collective/js/supersized/browser/views.py
+++ collective/js/supersized/browser/views.py
@@ -5,9 +5,10 @@ from Products.Five import BrowserView
 from Products.CMFCore.utils import getToolByName
 #from zope.component import getMultiAdapter
 
+
 class SupersizedView(BrowserView):
     """
-    not sure if this is needed
+    A browser view to be used on news items. It will show the news image as background image as background
     """
     template = ViewPageTemplateFile('supersized.pt')
 
@@ -16,21 +17,6 @@ class SupersizedView(BrowserView):
 <script type="text/javascript" charset="utf-8">
 $(document).ready(function(){
     $.supersized({
-        // Functionality
-        slideshow               :   1,			// Slideshow on/off
-        autoplay				:	1,			// Slideshow starts playing automatically
-        start_slide             :   1,			// Start slide (0 is random)
-        stop_loop				:	0,			// Pauses slideshow on last slide
-        random					: 	0,			// Randomize slide order (Ignores start slide)
-        slide_interval          :   3000,		// Length between transitions
-        transition              :   6, 			// 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
-        transition_speed		:	1000,		// Speed of transition
-        new_window				:	1,			// Image links open in new window/tab
-        pause_hover             :   0,			// Pause slideshow on hover
-        keyboard_nav            :   1,			// Keyboard navigation on/off
-        performance				:	1,			// 0-Normal, 1-Hybrid speed/quality, 2-Optimizes image quality, 3-Optimizes transition speed // (Only works for Firefox/IE, not Webkit)
-        image_protect			:	1,			// Disables image dragging and right click with Javascript
-                                                   
         // Size & Position						   
         min_width		        :   0,			// Min width allowed (in pixels)
         min_height		        :   0,			// Min height allowed (in pixels)
@@ -43,12 +29,10 @@ $(document).ready(function(){
         // Components							
         slide_links				:	'blank',	// Individual links for each slide (Options: false, 'num', 'name', 'blank')
         thumb_links				:	1,			// Individual thumb links for each slide
-        thumbnail_navigation    :   0,			// Thumbnail navigation
         slides 					:  	[{image : '%(image)s/image'},
                                     ],
                                     
         // Theme Options			   
-        progress_bar			:	1,			// Timer for each slide							
         mouse_scrub				:	0
     });
 });

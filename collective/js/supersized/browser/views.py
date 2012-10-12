44933053ec313b4ce6ee98c9d3c2111f6ce7cf94
Espen Moe-Nilssen <espen@medialog.no>
2012-10-12 10:42:41 +0200

    Added view for news item and settings for js from property sheet

--- collective/js/supersized/browser/views.py
+++ collective/js/supersized/browser/views.py
@@ -6,48 +6,57 @@ from Products.CMFCore.utils import getToolByName
 #from zope.component import getMultiAdapter
 
 
+
 class SupersizedView(BrowserView):
     """
     A browser view to be used on news items. It will show the news image as background image as background
     """
     template = ViewPageTemplateFile('supersized.pt')
 
+    def __init__(self, context):
+        """ init view """
+        self.context = context
+        propertiestool = getToolByName(self.portal, 'portal_properties')
+        supersized_properties = getattr(propertiestool, 'supersized_properties', None)
+    
     def javascript(self):
         return u"""
-<script type="text/javascript"
-    src="++resource++supersized.min.js"></script>
-<script type="text/javascript"
-    src="++resource++supersized.shutter.min.js"></script>
 <script type="text/javascript" charset="utf-8">
 $(document).ready(function(){
     $.supersized({
         // Size & Position						   
-        min_width		        :   0,			// Min width allowed (in pixels)
-        min_height		        :   0,			// Min height allowed (in pixels)
-        vertical_center         :   1,			// Vertically center background
-        horizontal_center       :   1,			// Horizontally center background
-        fit_always				:	0,			// Image will never exceed browser width or height (Ignores min. dimensions)
-        fit_portrait         	:   0,			// Portrait images will not exceed browser height
-        fit_landscape			:   0,			// Landscape images will not exceed browser width
+        min_width		        :   %(min_width)i,			// Min width allowed (in pixels)
+        min_height		        :   %(min_heitgh)i,			// Min height allowed (in pixels)
+        vertical_center         :   %(vertical_center)i,	// Vertically center background
+        horizontal_center       :   %(horizontal_center)i,	// Horizontally center background
+        fit_always				:	$%(fit_always)i,		// Image will never exceed browser width or height (Ignores min. dimensions)
+        fit_portrait         	:   %(fit_portrait)i,		// Portrait images will not exceed browser height
+        fit_landscape			:   %(fit_landscape)i,		// Landscape images will not exceed browser width
                                                    
         // Components							
         slide_links				:	'blank',	// Individual links for each slide (Options: false, 'num', 'name', 'blank')
-        thumb_links				:	1,			// Individual thumb links for each slide
+        thumb_links				:	%(thumb_links),			// Individual thumb links for each slide
         slides 					:  	[{image : '%(image)s/image'},
                                     ],
                                     
         // Theme Options			   
-        mouse_scrub				:	0
+        mouse_scrub				:	%(mouse_scrub)i,
     });
 });
 </script>
 """ % {
         'image' : self.context.absolute_url(),
+        'min_width'	: getattr(self.prettyphoto_properties, 'min_width', 0),
+        'min_height' : getattr(self.prettyphoto_properties, 'min_height', 0),
+        'vertical_center' : getattr(self.prettyphoto_properties, 'vertical_center', 1),
+        'horizontal_center' : getattr(self.prettyphoto_properties, 'horizontal_center', 1),
+        'fit_always' : getattr(self.prettyphoto_properties, 'fit_always', 0),
+        'fit_portrait' : getattr(self.prettyphoto_properties, 'fit_portrait', 0),
+        'fit_landscape'	: getattr(self.prettyphoto_properties, 'fit_landscape', 0),
+        'thumb_links' : getattr(self.prettyphoto_properties, 'thumb_lins', 1),
+        'mouse_scrub' : getattr(self.prettyphoto_properties, 'mouse_scrub', 0),
     }
-
 
     def css(self):
         return u"""
-<link rel="stylesheet" type="text/css" href="++resource++supersized.css"/>
-<link rel="stylesheet" type="text/css" href="++resource++supersized.shutter.css"/>
 """ 
